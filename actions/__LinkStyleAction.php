abc14a4ec6d22ea87bc13bfa55c5b5dafd812dc3
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-07-05 19:39:44 +0200

    fix(style): position of facettes

---
+++ actions/__LinkStyleAction.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the YesWiki Extension alternativeupdatej9rem.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace YesWiki\Alternativeupdatej9rem;
+
+use YesWiki\Core\Service\AssetsManager;
+use YesWiki\Core\YesWikiAction;
+
+class __LinkStyleAction extends YesWikiAction
+{
+    public function run()
+    {
+        $this->getService(AssetsManager::class)->AddCSSFile('tools/alternativeupdatej9rem/styles/fix.css');
+    }
+}
