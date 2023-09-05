ea0274ec5f04920326d06d33a0bb14d4101cb2fd
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-09-05 15:46:40 +0200

    fix(EditHandler): allow register tag

---
+++ handlers/EditHandler__.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace YesWiki\Alternativeupdatej9rem;
+
+use YesWiki\Core\Service\AclService;
+use YesWiki\Core\YesWikiHandler;
+
+class EditHandler__ extends YesWikiHandler
+{
+    public function run()
+    {
+        // get services
+        $aclService = $this->getService(AclService::class);
+        $release = $this->params->has('yeswiki_release') ? $this->params->get('yeswiki_release') : '';
+        $release = !is_string($release) ? '' : $release;
+        if (!preg_match('/^4\.(?:[0-3]\.[0-9]|4\.0)+$/',$release)){
+            return;
+        }
+
+        if (
+            !$this->params->get('hide_keywords')
+            && $aclService->hasAccess("write")
+        ){
+                
+            // clean output
+            $matches = [];
+            if (preg_match('/<script>[^<]+var tagsexistants[^<]+<\/script>/',$this->output,$matches)){
+                $this->output = str_replace(
+                    $matches[0],
+                    '',
+                    $this->output
+                );
+            }
+        }
+    }
+}
