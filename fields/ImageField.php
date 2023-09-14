6590b5eb8dad0c32862af5ea7420117a21c2bd9c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-09-14 10:50:18 +0200

    fix(ImageField): be carefull when saving previous value

---
+++ fields/ImageField.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the YesWiki Extension alternativeupdatej9rem.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace YesWiki\Alternativeupdatej9rem\Field;
+
+use YesWiki\Alternativeupdatej9rem\Service\DateService;
+use YesWiki\Bazar\Field\ImageField as CoreImageField;
+
+/**
+ * changes only for `doryphore 4.4.1`
+ */
+/**
+ * @Field({"image"})
+ */
+class ImageField extends CoreImageField
+{
+    public function formatValuesBeforeSave($entry)
+    {
+        $value = $this->getValue($entry);
+        if ((!empty($_FILES[$this->propertyName]['name']) && !empty($entry['id_fiche']))
+            || (isset($entry['oldimage_' . $this->propertyName]) && $entry['oldimage_' . $this->propertyName] != '')) {
+            return parent::formatValuesBeforeSave($entry);
+        } elseif (!empty($value)) {
+            $entry[$this->propertyName] = file_exists($this->getBasePath(). $this->getValue($entry)) ? $this->getValue($entry) : '';
+        } else {
+            $entry[$this->propertyName] = '';
+        }
+        return [
+            $this->propertyName => $this->getValue($entry),
+            'fields-to-remove' => ['oldimage_' . $this->propertyName]
+        ];
+    }
+}
