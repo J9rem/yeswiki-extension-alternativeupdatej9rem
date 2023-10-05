53fd2d84043f1dedd19f3f38df130c7d896836c4
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-10-05 11:42:12 +0200

    fix(DuplicateButton): does not show for doryphore 4.4.3

--- actions/BarreRedactionAction__.php
+++ actions/BarreRedactionAction__.php
@@ -18,17 +18,21 @@ class BarreRedactionAction__ extends YesWikiAction
 {
     public function run()
     {
-        $anchor = preg_quote('class="link-edit"><i class="fa fa-pencil-alt"></i><span>'. html_entity_decode(_t('TEMPLATE_EDIT_THIS_PAGE')) .'</span></a>','/');
-        $anchor = str_replace(
-            '>',
-            '>\\s*',
-            $anchor);
-        $button = '<a title="' . _t('AUJ9_DUPLICATE') . '" href="' . $this->wiki->Href('duplicate'.testUrlInIframe()) . '"><i class="fas fa-copy"></i></a>';
-        $match = [];
-        $this->output = preg_replace(
-            "/($anchor)/",
-            "$1$button",
-            $this->output
-        );
+        $release = $this->params->get('yeswiki_release');
+        if (is_string($release)
+            && preg_match('/^4\.(?:[0-3]\.[0-9]|4\.[0-2])$/',$release)){
+            $anchor = preg_quote('class="link-edit"><i class="fa fa-pencil-alt"></i><span>'. html_entity_decode(_t('TEMPLATE_EDIT_THIS_PAGE')) .'</span></a>','/');
+            $anchor = str_replace(
+                '>',
+                '>\\s*',
+                $anchor);
+            $button = '<a title="' . _t('AUJ9_DUPLICATE') . '" href="' . $this->wiki->Href('duplicate'.testUrlInIframe()) . '"><i class="fas fa-copy"></i></a>';
+            $match = [];
+            $this->output = preg_replace(
+                "/($anchor)/",
+                "$1$button",
+                $this->output
+            );
+        }
     }
 }
