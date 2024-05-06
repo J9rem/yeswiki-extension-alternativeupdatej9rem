aed0202b329a446e35daca75556c72d5ca37799f
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2024-05-06 09:40:27 +0200

    Revert "refactor(DbService): use parent construct"
    This reverts commit 09ae9ffe40151cf07f2451fdda2c6eecb037e8ad.

--- services/DbService.php
+++ services/DbService.php
@@ -13,6 +13,7 @@
 namespace YesWiki\Alternativeupdatej9rem\Service;
 
 use Exception;
+use mysqli;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Throwable;
 use YesWiki\Core\Service\DbService as CoreDbService;
@@ -21,8 +22,41 @@ class DbService extends CoreDbService
 {
     public function __construct(ParameterBagInterface $params)
     {
-        parent::__construct($params);
-        $this->setSessionTimeout();
+        $this->params = $params;
+        $this->queryLog = [];
+        try {
+            $this->link = $this->prepareMySqli();
+            $this->setSqlSessionConnectTimeout();
+            if (!$this->connect()) {
+                throw new Exception("Not connected to sql");
+            }
+            $this->setCharset();
+            $this->setSessionTimeout();
+        } catch (Throwable $th) {
+            if (in_array(php_sapi_name(), ['cli', 'cli-server',' phpdbg'], true)) {
+                throw new Exception(_t('DB_CONNECT_FAIL') . " {$th->getMessage()}");
+            } else {
+                exit(_t('DB_CONNECT_FAIL') . " {$th->getMessage()}");
+            }
+        }
+    }
+
+    /**
+     * prepare sqli like in CoreDbService
+     * @return mysqli
+     * @throws Exception
+     */
+    protected function prepareMySqli(): mysqli
+    {
+        /**
+         * @var mysqli $link
+         */
+        $link = mysqli_init();
+
+        if (!$link) {
+            throw new Exception("SQL not initiated !");
+        }
+        return $link;
     }
 
     /**
@@ -37,6 +71,56 @@ class DbService extends CoreDbService
         $wantedTimeout = $this->params->get('sqlConnectTimeout');
         $wantedTimeout = (!empty($wantedTimeout) && is_scalar($wantedTimeout)) ? intval($wantedTimeout) : 0;
         return $wantedTimeout;
+    }
+
+    /**
+     * set sql session's connect  timeout if required
+     * @return void
+     */
+    protected function setSqlSessionConnectTimeout()
+    {
+        /**
+         * @var mixed $wantedTimeout
+         */
+        $wantedTimeout = $this->getWantedTimeout();
+        if ($wantedTimeout > 0) {
+            mysqli_options($this->link, MYSQLI_OPT_CONNECT_TIMEOUT, $wantedTimeout);
+            mysqli_options($this->link, MYSQLI_OPT_READ_TIMEOUT, $wantedTimeout);
+        }
+    }
+
+    /**
+     * protected connect
+     * @return bool true of ok
+     */
+    protected function connect(): bool
+    {
+        return mysqli_real_connect(
+            $this->link,
+            $this->params->get('mysql_host'),
+            $this->params->get('mysql_user'),
+            $this->params->get('mysql_password'),
+            $this->params->get('mysql_database'),
+            $this->params->has('mysql_port') ? $this->params->get('mysql_port') : ini_get("mysqli.default_port")
+        );
+    }
+
+    /**
+     * set charset
+     * @return void
+     */
+    protected function setCharset()
+    {
+        if ($this->params->has('db_charset') and $this->params->get('db_charset') === 'utf8mb4') {
+            // necessaire pour les versions de mysql qui ont un autre encodage par defaut
+            mysqli_set_charset($this->link, 'utf8mb4');
+
+            // dans certains cas (ovh), set_charset ne passe pas, il faut faire une requete sql
+            $charset = mysqli_character_set_name($this->link);
+            if ($charset != 'utf8mb4') {
+                mysqli_query($this->link, 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci');
+            }
+        }
     }
 
     /**
